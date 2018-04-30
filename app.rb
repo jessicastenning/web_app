967656dd1af16b12e8dd3fd55bcf227e67774242
jessica stenning <jessicastenning@hotmail.co.uk>
2018-04-30 16:00:35 +0100

    adds params for 'named-cat' route

--- app.rb
+++ app.rb
@@ -16,6 +16,13 @@ get '/route2' do
   "Test2!"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @random_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @random_name = params[:name]
   erb(:index)
 end
